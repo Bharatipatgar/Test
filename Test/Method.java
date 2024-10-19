36909ef27475aaaa35f33373eb417085b2375d64
Bharatipatgar <155898014+Bharatipatgar@users.noreply.github.com>
2024-10-19 13:39:17 +0530

    Add files via upload

---
+++ Test/Method.java
@@ -0,0 +1,24 @@
+public class Method {
+public static void main(String[] args) {
+
+add();
+multiply();
+
+}
+
+public static void add(){
+int value1=10;
+int value2=20;
+int addition=value1+value2;
+System.out.println("addition:"+ addition);
+}
+public static void multiply(){
+int value1=10;
+int value2=20;
+int multiplication= value1*value2;
+System.out.println("multiplication:"+multiplication);
+}
+
+}
+
+
---
+++ Test/arrayex.java
@@ -0,0 +1,30 @@
+public class arrayex{
+public static void main(String args[]){
+
+
+ int[] number = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3};
+ for(int i=0;i<20;i++){
+ System.out.println(number[i]);
+}
+int[] price = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3};
+for(int i=20;i>=0;i--){
+ System.out.println(price[i]);
+}
+
+int[] decimal = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3};
+for(int i=10;i>=0;i--){
+ System.out.println(decimal[i]);
+}
+int[] no = {1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3};
+for(int i=0;i<=20;i=i+2){
+ System.out.println(no[i]);
+}
+}
+}
+
+
+
+
+
+
+
