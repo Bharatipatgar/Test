aa8c9914095328570168e61457da60af45e35c00
Bharatipatgar <155898014+Bharatipatgar@users.noreply.github.com>
2024-10-19 14:24:48 +0530

    Add files via upload

---
+++ Test/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+
+    String employeeId;
+    String name;
+    String role;
+    double salary;
+    boolean isOnDuty;
+   
+    public static void startShift(){
+        
+    }
+   
+    public static void endShift(){
+            }
+   
+    public static void viewEmployeeDetails(){
+        
+    }
+   
+    public static void assignBus(){
+            }
+}
